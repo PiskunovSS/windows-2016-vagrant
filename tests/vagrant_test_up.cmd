56e4bd2532fa49fbb75cfad5206f12726cda5865
Сергей Пискунов <s.piskunov@ant-konsalt.ru>
2018-03-28 11:49:55 +0300

    #1 Почему-то vagrant up не работает сразу после box add. Приходится открывать новое окно команд и тогда все норм. Поэтому добавил вызов через start /WAIT /b Информация взятат отсюда https://www.celitel.info/klad/nhelp/helpbat.php?dcmd=start

--- tests/vagrant_test_up.cmd
+++ tests/vagrant_test_up.cmd
@@ -2,13 +2,13 @@
 chcp 65001
 
 echo 2. Добавляем бокс в vagrant под именем windows-2016-amd64
-call vagrant box add -f windows-2016-amd64 windows-2016-amd64-virtualbox.box
+vagrant box add -f windows-2016-amd64 windows-2016-amd64-virtualbox.box
 if NOT %ERRORLEVEL% == 0 ( echo Vagrant test up error & EXIT /B 1)
-
+TIMEOUT /T 60
 
 echo 3. Создаем тестовую машину из бокса
 cd example
-call vagrant up --provider=virtualbox
+start /WAIT /b vagrant up --provider=virtualbox
 if NOT %ERRORLEVEL% == 0 ( echo Vagrant test up error & EXIT /B 1)
 
 
@@ -16,14 +16,11 @@ echo 4. Проверяем работу WinRM
 @powershell ../tests/check_winrm_and_provision.ps1
 if NOT %ERRORLEVEL% == 0 ( echo Vagrant test up error & EXIT /B 1)
 
-echo 5. Выключаем тестовую машину
-call vagrant halt
-
-echo 6. Удаляем тестовую машину
-call vagrant destroy -f
+echo 5. Удаляем тестовую машину
+vagrant destroy -f
 if NOT %ERRORLEVEL% == 0 ( echo Vagrant test up error & EXIT /B 1)
 
-echo 7. Удаляем тестовый бокс из vagrant
-call vagrant box remove windows-2016-amd64 -f
+echo 6. Удаляем тестовый бокс из vagrant
+vagrant box remove windows-2016-amd64 -f
 if NOT %ERRORLEVEL% == 0 ( echo Vagrant test up error & EXIT /B 1)
 echo Vagrant test up OK	
