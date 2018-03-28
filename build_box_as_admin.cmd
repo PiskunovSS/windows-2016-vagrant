c355ce8c07e8d922e0f545f6668fba1b57d4e6b4
Сергей Пискунов <s.piskunov@ant-konsalt.ru>
2018-03-28 13:14:41 +0300

    #1 Оказывается для vagrant up важна кодировка

--- build_box_as_admin.cmd
+++ build_box_as_admin.cmd
@@ -4,6 +4,7 @@ chcp 65001
 echo 1. Устанавливаем ОС из официального ISO образа, настраиваем и создаем vagrant box
 
 cd /d %~dp0
-::call packer build -only=windows-2016-amd64-virtualbox windows-2016.json
+chcp 866
+packer build -only=windows-2016-amd64-virtualbox windows-2016.json
 if NOT %ERRORLEVEL% == 0 ( echo Packer build error & EXIT /B 1)
 echo Build OK
--- tests/vagrant_test_up.cmd
+++ tests/vagrant_test_up.cmd
@@ -1,6 +1,9 @@
 @echo off
 chcp 65001
 
+cd /d %~dp0
+cd ..
+
 echo 2. Добавляем бокс в vagrant под именем windows-2016-amd64
 vagrant box add -f windows-2016-amd64 windows-2016-amd64-virtualbox.box
 if NOT %ERRORLEVEL% == 0 ( echo Vagrant test up error & EXIT /B 1)
@@ -8,9 +11,10 @@ TIMEOUT /T 60
 
 echo 3. Создаем тестовую машину из бокса
 cd example
-start /WAIT vagrant up --provider=virtualbox
+chcp 866
+vagrant up --provider=virtualbox
 if NOT %ERRORLEVEL% == 0 ( echo Vagrant test up error & EXIT /B 1)
-
+chcp 65001
 
 echo 4. Проверяем работу WinRM
 @powershell ../tests/check_winrm_and_provision.ps1
